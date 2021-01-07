78ca82f38aa2034c8c4964c595b7cbe7b85f1aac
shin.imac <shintec@gmail.com>
2021-01-07 23:57:29 +0900

    code refactoring

--- Assets/Scripts/LineGenerator.cs
+++ Assets/Scripts/LineGenerator.cs
@@ -4,168 +4,67 @@ using UnityEngine;
 
 public class LineGenerator : MonoBehaviour
 {
-    [SerializeField] float initRadius = 300f;
-    [SerializeField] UnityEngine.UI.Button buttonSplit = default;
-    [SerializeField] UnityEngine.UI.Button buttonMerge = default;
-    [SerializeField] UnityEngine.UI.Text labelStep = default;
     [SerializeField] float minWidth = 0.018f;
     [SerializeField] float fadeTime = 0.4f;
 
     private LineRenderer line = default;
-    private List<Vector3> positions = new List<Vector3>();
-    private List<Vector3> prevPositions = new List<Vector3>();
+    private Vector3[] startCurve;
+    private Vector3[] endCurve;
     private float lineWidth = 0.5f;
-    private int iterationStep = 1;
     private bool isInFade = false;
 
-    public float InitRadius => initRadius;
+    public bool IsInFade => isInFade;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        buttonSplit.onClick.AddListener(() => Split());
-        buttonMerge.onClick.AddListener(() => Merge());
-
         line = GetComponent<LineRenderer>();
-        positions.Clear();
-        positions.Add(new Vector3(-initRadius, initRadius / 2, 0));
-        positions.Add(new Vector3(0, -initRadius / 2, 0));
-        positions.Add(new Vector3(initRadius, initRadius / 2, 0));
-        line.positionCount = positions.Count;
-        line.SetPositions(positions.ToArray());
-
-        labelStep.text = "Iteration : " + iterationStep.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateLine(Vector3[] curve, float lineWidth)
     {
-        
+        line.positionCount = curve.Length;
+        line.SetPositions(curve);
+        line.widthMultiplier = Mathf.Max(minWidth, lineWidth);
     }
 
-    public void Split()
+    public void UpdateLine(Vector3[] startCurve, Vector3[] endCurve, float lineWidth)
     {
-        if (isInFade || iterationStep >= 15) return;
-
-        iterationStep++;
-
-        float sqrt2 = Mathf.Sqrt(2f);
-        int iterations = positions.Count - 1;
-        int index = 1;
-        bool foward = false;
-        prevPositions = new List<Vector3>(positions); // clone list
-
-        for (int i = 0; i < iterations; i++)
-        {
-            Vector3 midPoint = Vector3.Lerp(positions[index-1], positions[index], 0.5f);
-            prevPositions.Insert(index, midPoint);
-
-            Vector3 offset = positions[index] - positions[index - 1];
-            offset = Quaternion.AngleAxis(45, foward ? Vector3.forward : Vector3.back) * offset;
-            Vector3 offset2 = offset.normalized * offset.magnitude / sqrt2;
-            positions.Insert(index, positions[index - 1] + offset2);
-            index += 2;
-            foward = !foward;
-        }
+        this.startCurve = startCurve;
+        this.endCurve = endCurve;
+        this.lineWidth = lineWidth;
 
         isInFade = true;
-        StartCoroutine(ProcSplit());
-
-        labelStep.text = "Iteration : " + iterationStep.ToString();
+        StartCoroutine(ProcUpdateLine());
     }
 
-    IEnumerator ProcSplit()
+    IEnumerator ProcUpdateLine()
     {
-        line.positionCount = prevPositions.Count;
-        line.SetPositions(prevPositions.ToArray());
+        int size = startCurve.Length;
+        line.positionCount = size;
+        line.SetPositions(startCurve);
 
-        List<Vector3> animatePositions = new List<Vector3>(prevPositions); // clone list
+        Vector3[] curve = new Vector3[size];
+        System.Array.Copy(startCurve, curve, size);
         float w = 0f;
-        int iterations = (positions.Count - 1) / 2;
+        int iterations = (size - 1) / 2;
 
         while (w < 1f)
         {
             int index = 1;
             for (int i = 0; i < iterations; i++)
             {
-                animatePositions[index] = Vector3.Lerp(prevPositions[index], positions[index], w);
+                curve[index] = Vector3.Lerp(startCurve[index], endCurve[index], w);
                 index += 2;
             }
 
-            line.SetPositions(animatePositions.ToArray());
+            line.SetPositions(curve);
             yield return null;
             w += Time.deltaTime / fadeTime;
         }
 
-        line.positionCount = positions.Count;
-        line.SetPositions(positions.ToArray());
-        lineWidth = lineWidth / 1.4f;
+        line.positionCount = endCurve.Length;
+        line.SetPositions(endCurve);
         line.widthMultiplier = Mathf.Max(minWidth, lineWidth);
         isInFade = false;
-    }
-
-    public void Merge()
-    {
-        if (isInFade || iterationStep <= 1) return;
-
-        iterationStep--;
-
-        prevPositions = new List<Vector3>(positions); // clone list
-
-        int iterations = (positions.Count - 1) / 2;
-        int index = 1;
-        for (int i = 0; i < iterations; i++)
-        {
-            positions[index] = Vector3.Lerp(positions[index - 1], positions[index + 1], 0.5f);
-            index += 2;
-        }
-
-        isInFade = true;
-        StartCoroutine(ProcMerge());
-    }
-
-    IEnumerator ProcMerge()
-    {
-        line.positionCount = prevPositions.Count;
-        line.SetPositions(prevPositions.ToArray());
-
-        List<Vector3> animatePositions = new List<Vector3>(prevPositions); // clone list
-        float w = 0f;
-        int iterations = (positions.Count - 1) / 2;
-
-        while (w < 1f)
-        {
-            int index = 1;
-            for (int i = 0; i < iterations; i++)
-            {
-                animatePositions[index] = Vector3.Lerp(prevPositions[index], positions[index], w);
-                index += 2;
-            }
-
-            line.SetPositions(animatePositions.ToArray());
-            yield return null;
-            w += Time.deltaTime / fadeTime;
-        }
-
-        EndMerge();
-
-        isInFade = false;
-        labelStep.text = "Iteration : " + iterationStep.ToString();
-    }
-
-    void EndMerge()
-    {
-        int iterations = (positions.Count - 1) / 2;
-        int index = 1;
-        for (int i = 0; i < iterations; i++)
-        {
-            positions.RemoveAt(index);
-            index += 1;
-        }
-
-        line.positionCount = positions.Count;
-        line.SetPositions(positions.ToArray());
-        lineWidth = lineWidth * 1.4f;
-        line.widthMultiplier = Mathf.Max(minWidth, lineWidth);
     }
 }
---
+++ Assets/Scripts/SceneOperator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneOperator : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Button buttonSplit = default;
+    [SerializeField] UnityEngine.UI.Button buttonMerge = default;
+    [SerializeField] UnityEngine.UI.Text labelIteration = default;
+    [SerializeField] LineGenerator lineGenerator = default;
+    [SerializeField] float initRadius = 300f;
+
+    private DragonCurve dragonCurve = null;
+    private Dictionary<int, Vector3[]> curveTable = new Dictionary<int, Vector3[]>();
+    private Dictionary<int, Vector3[]> lineTable = new Dictionary<int, Vector3[]>();
+    private Dictionary<int, float> widthTable = new Dictionary<int, float>();
+
+    private int currentIteration = 1;
+    public readonly int kMaxIteration = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonSplit.onClick.AddListener(() =>
+        {
+            if (currentIteration >= kMaxIteration || lineGenerator.IsInFade) return;
+            currentIteration++;
+            UpdateIterationLabel();
+            lineGenerator.UpdateLine(lineTable[currentIteration], curveTable[currentIteration], widthTable[currentIteration]);
+        });
+        buttonMerge.onClick.AddListener(() =>
+        {
+            if (currentIteration <= 1 || lineGenerator.IsInFade) return;
+            lineGenerator.UpdateLine(curveTable[currentIteration], lineTable[currentIteration], widthTable[currentIteration]);
+            currentIteration--;
+            UpdateIterationLabel();
+        });
+
+        dragonCurve = new DragonCurve(false);
+
+        // pre calculate curves for all iteration
+        List<Vector3> curve = new List<Vector3>();
+        curve.Add(new Vector3(-initRadius, initRadius / 2, 0));
+        curve.Add(new Vector3(0, -initRadius / 2, 0));
+        curve.Add(new Vector3(initRadius, initRadius / 2, 0));
+        lineTable[1] = curve.ToArray();
+        curveTable[1] = curve.ToArray();
+
+        float lineWidth = 0.5f;
+        widthTable[1] = lineWidth;
+
+        for (int i = 2; i <= kMaxIteration; i++)
+        {
+            List<Vector3> splitLine = dragonCurve.SplitByLine(curve);
+            List<Vector3> splitCurve = dragonCurve.SplitByCurve(curve);
+            lineTable[i] = splitLine.ToArray();
+            curveTable[i] = splitCurve.ToArray();
+
+            lineWidth = lineWidth / 1.4f;
+            widthTable[i] = lineWidth;
+
+            curve = splitCurve;
+        }
+
+        currentIteration = 1;
+        UpdateIterationLabel();
+        lineGenerator.UpdateLine(curveTable[currentIteration], widthTable[currentIteration]);
+    }
+
+    void UpdateIterationLabel()
+    {
+        labelIteration.text = "Iteration : " + currentIteration.ToString();
+    }
+}
